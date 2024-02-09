0b7973d540c6b31fd3c148bac4459fcbc25337f1
D1rosa <151547045+D1rosa@users.noreply.github.com>
2024-02-09 19:52:19 +0400

    Add files via upload

--- web.py
+++ web.py
@@ -10,5 +10,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
