232226b966b402f38578ec88b05a0cece0cdda84
D1rosa <151547045+D1rosa@users.noreply.github.com>
2024-02-09 19:20:39 +0400

    Add files via upload

--- web.py
+++ web.py
@@ -10,26 +10,5 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/odd_even')
-def odd_even():
-    return render_template('odd_even.html', number=2)
-
-
-@app.route('/news')
-def news():
-    with open("news.json", "rt", encoding="utf8") as f:
-        news_list = json.loads(f.read())
-    print(news_list)
-    return render_template('news.html', news=news_list)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('login.html', title='Авторизация', form=form)
-
-
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
