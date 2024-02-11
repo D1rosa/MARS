91fea08678e110d4a1dad6a8b3d1cc2ae0b5b174
D1rosa <151547045+D1rosa@users.noreply.github.com>
2024-02-11 23:18:40 +0400

    Add files via upload

--- web.py
+++ web.py
@@ -1,5 +1,5 @@
 from flask import Flask, url_for, request, render_template, json, redirect
-from login import LoginForm
+# from login import LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
@@ -13,6 +13,11 @@ def index():
 @app.route('/training/<prof>')
 def training(prof):
     return render_template('training.html', prof=prof)
+
+
+@app.route('/list_prof')
+def list_prof():
+    return render_template('list_prof.html',)
 
 
 if __name__ == '__main__':
